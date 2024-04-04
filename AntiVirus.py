3be4b9632f2a29f4247d472b86a16d463dd2f933
Alexander Denisenko <sashadenisenko6@gmail.com>
2024-04-04 23:19:16 +0300

    Add comments

--- AntiVirus.py
+++ AntiVirus.py
@@ -1,18 +1,24 @@
 import requests, time
 
 def main():
+    #Input of the file path by the user.                                 
     file_path = input("Enter the file path: ")
 
+    #The JSON file of the scan resulte
     scan_result = post_file_scan(file_path)
-    if scan_result['response_code'] == 1:  # Check if the file was successfully scanned
+    # Check if the file was successfully scanned
+    if scan_result['response_code'] == 1:  
+        #From the JSON file show the value of the key "scan_id"
         scan_id = scan_result['scan_id']
+        #print the Scan ID
         print(f"Scan ID: {scan_id}")
         # Check scan status periodically until it's completed
         while True:
             scan_status = check_scan_status(scan_id)
-            if scan_status == 'completed':
-                
+            if scan_status == 'completed':         
                 scan_report = get_file_scan(scan_id)
+                #Check if there are any viruses detected
+                #When the value of the key "positives" is 0, the file is "clean"
                 if(scan_report['positives']==0):
                     print("All clean, no viruses detected!")
                     time.sleep(3)
@@ -25,6 +31,7 @@ def main():
                     print("Scan report:")
                     print(scan_report)
                 break
+            #When the scan request is in queue, check the status every 30 seconds
             elif scan_status == 'queued' or scan_status == 'in-progress':
                 print("Scan is still in progress. Waiting for completion...")
                 time.sleep(30)  # Wait for 30 seconds before checking again
@@ -34,7 +41,7 @@ def main():
     else:
         print("File scan failed.")
 
-
+#Function to send the file to Virustotal using the methon "post"
 def post_file_scan(file_path):
     url = 'https://www.virustotal.com/vtapi/v2/file/scan'
     API_key = '9326b2dce9d84fc0981c5f62bfb7234ee78ba7cded80d19d77a2e33f41ad9e82'
@@ -42,17 +49,20 @@ def post_file_scan(file_path):
     with open(file_path,'rb') as file:
         files = {'file':(file_path, file)}
         response = requests.post(url,files=files, params=params)
+    #Return a JSON of the posting data to get scan_id
     return response.json()
 
+#Function to get back the result of the virus scan usin the method "get"
 def get_file_scan(scan_id):
-    
     resource = scan_id
     url = 'https://www.virustotal.com/vtapi/v2/file/report'
     API_key = '9326b2dce9d84fc0981c5f62bfb7234ee78ba7cded80d19d77a2e33f41ad9e82'
     params = {'apikey':API_key, 'resource':resource}
     response = requests.get(url,params=params)
+    #Return a JSON of the scan resulte
     return response.json()
 
+#Function to check the status of the scanning while waiting it to complete
 def check_scan_status(scan_id):
     url = 'https://www.virustotal.com/vtapi/v2/file/report'
     API_key = '9326b2dce9d84fc0981c5f62bfb7234ee78ba7cded80d19d77a2e33f41ad9e82'
